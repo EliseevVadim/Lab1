102e4714b75293634c9c2103638640d81679209b
EliseevVadim <eliseevv02@mail.ru>
2019-12-25 18:21:12 +0300

    T3

---
+++ 1.3/задание 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h, m, s;
+            double corner;
+            h = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            s = int.Parse(Console.ReadLine());
+            corner = (((360) * (3600 * h + 60 * m + s)) / (3600 * 12));
+            Console.WriteLine(corner);
+            Console.ReadKey();
+
+        }
+    }
+}
