a0408044ebe67e5ee61b9a5dd5f0aa9c7f5d0574
EliseevVadim <eliseevv02@mail.ru>
2019-12-25 18:26:58 +0300

    T7

---
+++ 1.7/задание 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, d, c, b, reversed;
+            a = int.Parse(Console.ReadLine());
+            b = a / 100;
+            d = a % 10;
+            c = (a % 100) / 10;
+            reversed = d * 100 + c * 10 + b;
+            Console.WriteLine(reversed);
+            Console.ReadKey();
+        }
+    }
+}
