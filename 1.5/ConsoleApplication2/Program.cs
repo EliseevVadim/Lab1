1ace4c01c0a644bb02407dc7a409a1b930a5feca
EliseevVadim <eliseevv02@mail.ru>
2019-12-25 18:24:42 +0300

    T5

---
+++ 1.5/ConsoleApplication2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double katet1;
+            double katet2;
+            double sqare;
+            double perimeter;
+            katet1 = double.Parse(Console.ReadLine());
+            katet2 = double.Parse(Console.ReadLine());
+            sqare = (katet2 * katet1) / 2;
+            perimeter = katet1 + katet2 + Math.Sqrt(katet2 * katet2 + katet1 * katet1);
+            Console.WriteLine(sqare);
+            Console.WriteLine(perimeter);
+            Console.ReadKey();
+            
+
+            
+          
+            
+        }
+    }
+}
