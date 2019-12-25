7cc57d37aef3d775adde2cf8551740a59af03e7b
EliseevVadim <eliseevv02@mail.ru>
2019-12-25 18:26:08 +0300

    T6

---
+++ 1.6/задание 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c,d,e,answer;
+            a = int.Parse(Console.ReadLine());
+            b = a / 1000;
+            c = a % 10;
+            d = (a % 100)/10;
+            e = (a / 100)%10;
+            answer = b * c * d * e;
+            Console.WriteLine(answer);
+            Console.ReadKey();
+
+        }
+    }
+}
