7d2bc6b0b7ef7000895aeb80261246a9283a50aa
EliseevVadim <eliseevv02@mail.ru>
2019-12-25 18:08:14 +0300

    T2

---
+++ 1.2/задание 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, hours, minutes;
+            a = int.Parse(Console.ReadLine());
+            hours = a / 3600;
+            minutes = (a % 3600)/60;
+            Console.WriteLine(hours+ " hours");
+            Console.WriteLine(minutes+ " minutes");
+            Console.ReadKey();
+        }
+    }
+}
