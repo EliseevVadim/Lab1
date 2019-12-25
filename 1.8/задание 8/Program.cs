943948b11e9e80777750382b17bf2ba24b7ca610
EliseevVadim <eliseevv02@mail.ru>
2019-12-25 18:27:34 +0300

    T8

---
+++ 1.8/задание 8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, x;
+            x = int.Parse(Console.ReadLine());
+            a = x * x;
+            Console.WriteLine((a + x + 1) * (3 * a - 8 * x + 7));
+            Console.ReadKey();
+        }
+    }
+}
