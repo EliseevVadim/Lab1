8ea6e17d516b4673b61c87d9e3ea879384f887ce
EliseevVadim <eliseevv02@mail.ru>
2019-12-25 18:23:23 +0300

    T4

---
+++ 1.4/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
